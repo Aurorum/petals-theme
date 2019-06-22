64117953d094a21c2a2e71dcf74972bd516f0f61
Aurorum <torres126@clubpenguinmountains.com>
2019-06-22 21:42:46 +0100

    Update jetpack.php

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -4,7 +4,7 @@
  *
  * @link https://jetpack.com/
  *
- * @package Milkshake
+ * @package Petals
  */
 
 /**
@@ -14,11 +14,11 @@
  * See: https://jetpack.com/support/responsive-videos/
  * See: https://jetpack.com/support/content-options/
  */
-function milkshake_jetpack_setup() {
+function petals_jetpack_setup() {
 	// Add theme support for Infinite Scroll.
 	add_theme_support( 'infinite-scroll', array(
 		'container' => 'main',
-		'render'    => 'milkshake_infinite_scroll_render',
+		'render'    => 'petals_infinite_scroll_render',
 		'footer'    => 'page',
 	) );
 
@@ -28,7 +28,7 @@ function milkshake_jetpack_setup() {
 	// Add theme support for Content Options.
 	add_theme_support( 'jetpack-content-options', array(
 		'post-details'    => array(
-			'stylesheet' => 'milkshake-style',
+			'stylesheet' => 'petals-style',
 			'date'       => '.posted-on',
 			'categories' => '.cat-links',
 			'tags'       => '.tags-links',
@@ -42,12 +42,12 @@ function milkshake_jetpack_setup() {
 		),
 	) );
 }
-add_action( 'after_setup_theme', 'milkshake_jetpack_setup' );
+add_action( 'after_setup_theme', 'petals_jetpack_setup' );
 
 /**
  * Custom render function for Infinite Scroll.
  */
-function milkshake_infinite_scroll_render() {
+function petals_infinite_scroll_render() {
 	while ( have_posts() ) {
 		the_post();
 		if ( is_search() ) :
