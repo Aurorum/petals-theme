0c54ecb7892a8dff2f2213a81ffd79ccaf3a55f6
Aurorum <torres126@clubpenguinmountains.com>
2019-07-02 20:50:58 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -187,7 +187,7 @@ function petals_scripts() {
 
 	wp_enqueue_script( 'petals-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 	
-	wp_enqueue_style( 'petals-fonts', '//fonts.googleapis.com/css?family=Roboto|Montserrat');
+	wp_enqueue_style( 'petals-fonts', '//fonts.googleapis.com/css?family=Hind|Montserrat');
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
